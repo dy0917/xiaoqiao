aa9db07d88d4bef3d638fd19145469594f66aaac
kingsley <huangkingsley@gmail.com>
2015-04-06 11:59:57 +1200

    local push

--- app/scripts/controllers/product.js
+++ app/scripts/controllers/product.js
@@ -26,6 +26,12 @@ angular.module('xiaoqiaoApp')
                     $scope.$apply();
                 });
 
-            }
-
+            };
+            $scope.formatincome = function (income) {
+                var x = income.toString();
+                var pattern = /(-?\d+)(\d{3})/;
+                while (pattern.test(x))
+                    x = x.replace(pattern, "$1,$2");
+                return x;
+            };
         });
